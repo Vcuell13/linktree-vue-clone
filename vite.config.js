332ca104a939727a57e31a6175518dfe97dc5d10
Victor Cuellar <vcuell13@stu.southtexascollege.edu>
2026-09-20 23:51:16 -0500

    fixed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
@@ -13,3 +12,4 @@ export default defineConfig({
     },
   },
 })
+
