b9a4a426d62df7d9dbad0da8768379561073225c
Victor Cuellar <vcuell13@stu.southtexascollege.edu>
2026-09-20 22:38:07 -0500

    Changed App.vue ayout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex min-h-screen flex-col">
+  <div class="flex min-h-screen bg-linear-to-br from-gray-950 via-gray-850 to-gray-950 font-sans">
     <RouterView />
   </div>
 </template>
